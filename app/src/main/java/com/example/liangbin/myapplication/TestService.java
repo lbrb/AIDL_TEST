e52298c5c1b62027b4717b5cb2cbc0d484b19135
hasika <609498751@qq.com>
2017-11-06 22:12:37 +0800

    aaaa

--- app/src/main/java/com/example/liangbin/myapplication/TestService.java
+++ app/src/main/java/com/example/liangbin/myapplication/TestService.java
@@ -2,6 +2,7 @@ package com.example.liangbin.myapplication;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.IBinder;
 import android.os.RemoteException;
 
@@ -17,6 +18,7 @@ public class TestService extends Service {
     private final IBinder mBinder = new ITest.Stub() {
         @Override
         public int getPid() throws RemoteException {
+            BatteryManager
             return android.os.Process.myPid();
         }
     };
